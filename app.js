d1c6c612e0789995e6b07ca9fccbfa39e4ebc1d5
Denis Nikulin <denis.nikulin@motional.com>
2025-05-03 18:51:19 -0700

    finish rock-paper-scissor second stage of the project

--- app.js
+++ app.js
@@ -4,74 +4,73 @@ const btn_rock = document.querySelector("#rock");
 const btn_paper = document.querySelector("#paper");
 const btn_scissors = document.querySelector("#scissors");
 
-
-// step 1: getComputerChoice will randomly return one of the following values:
-// rock, paper or scissors. Use math.random
+const displayHumanScore = document.querySelector('.human-score');
+const displaComputerScore = document.querySelector('.computer-score');
 
 function getComputerChoice() {
     const computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
         case 0:
-            return 0;
+            return "rock";
         case 1:
-            return 1;
+            return "paper";
         case 2:
-            return 2;
+            return "scissors";
     }
 }
 
-// console.log(getComputerChoice());
-
-// step 2: write logic to get the human choice
-// write getHumanChoice 
-// use prompt to get user's input
-// no need for error handling
-function getHumanChoice() {
-    const humanChoice = prompt("please enter '0' for 'rock', '1' for 'paper', and '2' for 'scissors'");
-    return humanChoice
+function whoIsWinner(humanScore, computerScore) {
+    return result = humanScore > computerScore ? "human" : "computer";
 }
 
-// console.log(getHumanChoice())
-//step 3: declare the players score variables
 
-//step 4: write the logic to play a single round
+function displayScore(humanScore, computerScore) {
+    alert(`The final score is\n Human: ${humanScore}\n Computer: ${computerScore} 
+        \n The winner is ${whoIsWinner(humanScore, computerScore)}`)
+}
+
+let keepGoing = true;
+let counter = 0;
+
+btn_rock.addEventListener("click", () => {
+    const humanChoice = "rock";
+    playRound(humanChoice, getComputerChoice());
+    counter++;
+  //  console.log(`counter is ${counter}`);
+    if (counter === 5) {
+        displayScore(humanScore, computerScore);
+    }
+});
+
+btn_paper.addEventListener("click", () => {
+    const humanChoice = "paper";
+    playRound(humanChoice, getComputerChoice());
+    counter++;
+    if (counter === 5) {
+        displayScore(humanScore, computerScore);
+    }
+});
+
+btn_scissors.addEventListener("click", () => {
+    const humanChoice = "scissors";
+    playRound(humanChoice, getComputerChoice());
+    counter++;
+    if (counter === 5) {
+        displayScore(humanScore, computerScore);
+    }
+});
+
+
 function playRound(humanChoice, computerChoice) {
-    if ((humanChoice == 0 && computerChoice == 2) || (humanChoice == 1 && computerChoice == 0) || (humanChoice == 2 && computerChoice == 1) ) {
-        // console.log(humanChoice);
-        // console.log(computerChoice);
+    if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || (humanChoice == "rock" && computerChoice == "scissors") ) {
         humanScore = ++humanScore;
         console.log(`Player wins. Score is human: ${humanScore} computer: ${computerScore}`);
-    } else if ((humanChoice == 2 && computerChoice == 0) || (humanChoice == 0 && computerChoice == 1) || (humanChoice == 1 && computerChoice == 2) ) {
+        displayHumanScore.textContent = humanScore;
+    } else if ((humanChoice == "scissors" && computerChoice == "rock") || (humanChoice == "rock" && computerChoice == "paper") || (humanChoice == "paper" && computerChoice == "scissors") ) {
         computerScore = ++computerScore;
         console.log(`Computer wins. Score is human: ${humanScore} computer: ${computerScore}`);
+        displaComputerScore.textContent = computerScore;
     } else {
         console.log(`Draw. Score is human: ${humanScore} computer: ${computerScore}`);
     }
 }
-
-//step 5: write the logic for the entire game
-function playGame() {
-    let keepGoing = true;
-    let counter = 0;
-    while (keepGoing) {
-        temp = getHumanChoice();
-        alert(temp);
-        if (temp === "q") {
-           break;
-        }
-        playRound(getHumanChoice(), getComputerChoice());
-        displayScore(humanScore, computerScore);
-        counter++;
-        console.log(`Counter: ${counter}, type: ${typeof counter}`)
-
-    }
-    
-    displayScore(humanScore, computerScore);
-    alert("Gameover");
-}
-
-function displayScore(humanScore, computerScore) {
-    alert(`The score is\n Human: ${humanScore}\n Computer: ${computerScore}`)
-}
-
-playGame();
